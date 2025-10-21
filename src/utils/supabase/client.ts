2a4cd17ca918ffbcc3c8ec40bd2581b42e1b31de
Meshach Arinze <arinzemeshach123@gmail.com>
2025-10-21 15:23:05 +0100

    feat: add all utils to repo

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,20 @@
+import { createBrowserClient } from "@supabase/ssr";
+import { createClient as createNewClient } from "@supabase/supabase-js";
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+export const createAdminClient = () =>
+  createNewClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    }
+  );
