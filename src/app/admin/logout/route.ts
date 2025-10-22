6f20edccbe1e480b92a4af85ebf44f6d18b32d45
Meshach Arinze <arinzemeshach123@gmail.com>
2025-10-22 15:19:15 +0100

    refactor: remove next response

--- src/app/admin/logout/route.ts
+++ src/app/admin/logout/route.ts
@@ -1,12 +1,11 @@
 // ============================================
 // FILE: app/api/admin/logout/route.ts
 // ============================================
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-import { redirect } from 'next/navigation';
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function POST() {
   const cookieStore = await cookies();
-  cookieStore.delete('admin_auth');
-  redirect('/admin/leads');
+  cookieStore.delete("admin_auth");
+  redirect("/admin/leads");
 }
