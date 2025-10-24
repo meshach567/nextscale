6220d3ea4fbb2e73c304479ff3712fde4c0f8524
Meshach Arinze <arinzemeshach123@gmail.com>
2025-10-24 14:04:55 +0100

    style: add mono fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,22 +6,11 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-  display: "swap",
-  fallback: ["system-ui", "arial"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-  display: "swap",
-  fallback: [
-    "ui-monospace",
-    "SFMono-Regular",
-    "Consolas",
-    "Liberation Mono",
-    "Menlo",
-    "monospace",
-  ],
 });
 
 export const metadata: Metadata = {
