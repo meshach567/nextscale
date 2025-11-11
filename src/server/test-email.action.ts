a94ec50c8f7275e86c0948de6d4c4b00fb3ffc60
Meshach Arinze <arinzemeshach123@gmail.com>
2025-11-11 15:19:35 +0100

    feat: create a test email template

---
+++ src/server/test-email.action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { createElement } from 'react';
+import { sendEmail } from './email';
+import TestEmail from '@/components/emails/ContactEmail';
+
+export const sendTestEmailAction = async ({name}: {name: string}) => {
+  return await sendEmail({
+    to: 'recipient@mail.com',
+    subject: 'Test Email',
+    component: createElement(TestEmail,{
+        name: name
+    }),
+  });
+};
