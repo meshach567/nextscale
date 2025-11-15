8abcaf156125ac8450e73c94ba483e6c5f7b8f19
Meshach Arinze <arinzemeshach123@gmail.com>
2025-11-15 09:48:22 +0100

    refactor: add prisma to imprt

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { Prisma } from "@/generated/prisma/client";
-
+import {Prisma} from "@/generated/prisma/client";
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
